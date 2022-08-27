a653432255802c46e0bde73817159e092119af99
Yuliya <gothic8@mail.ru>
2022-08-27 12:21:14 +0300

    Example 12 Methods

---
+++ Example_012_Methods/Program.cs
@@ -0,0 +1,56 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+//Method1();
+
+// Вид 2  Ничего не принимают, но могут возвращать какие-то элементы:
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения");
+
+//или
+
+//void Method21(string msg, int count)
+//{
+//   int i = 0;
+//   while (i < count)
+//   {
+//       Console.WriteLine(msg);
+//        i++;
+//    }
+
+//}
+//Method21(msg: "Текст", 4);
+//или
+//Method21(count: 4, msg: "Новый текст");
+
+// Вид3: Методы, которые что-то возвращают, но ничего не принимают.
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+//Console.WriteLine(year);
+
+//Вид4: Методы, которые что-то принимают и что-то возвращают.
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
